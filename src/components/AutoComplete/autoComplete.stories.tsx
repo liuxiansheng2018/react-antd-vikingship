c34385173aa80eb2b3a6084e50463237355f6aaa
lh <liuxiansheng2014@sina.com>
2021-04-09 18:58:58 +0800

    a

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { AutoComplete } from './autoComplete'
-
-
 const SimpleComplete = () => {
   // const lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins',
   // 'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando']
