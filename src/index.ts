2059de9ee59402788bda1ffc18e7c1bac83d2a35
lh <liuxiansheng2014@sina.com>
2021-04-09 18:22:40 +0800

    fix: -m

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export { default as Menu } from './components/Menu'
 export { default as Icon } from './components/Icon'
 export { default as Input } from './components/Input'
 export { default as Transition } from './components/Transition'
-export { default as AutoComplete } from './components/AutoComplete'
+export { default as AutoComplete } from './components/AutoComplete';
